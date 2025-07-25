d2005b67884146598cd4bf88840c7b609376ebf1
Samuel Knight <robogamingbysam@icloud.com>
2025-07-25 01:55:29 -0500

    Rework structure to use Handlebars/Hono for SSR

--- main.ts
+++ main.ts
@@ -1,20 +1,39 @@
-import { Application } from "jsr:@oak/oak/application";
-import { Router } from "jsr:@oak/oak/router";
+import { Hono } from "hono";
+import { serveStatic } from "hono/deno";
+import Handlebars from "handlebars";
 
-const router = new Router();
-router.get("/", (ctx) => {
-  ctx.response.body = `<!DOCTYPE html>
-    <html>
-      <head><title>Hello oak!</title><head>
-      <body>
-        <h1>Hello oak!</h1>
-      </body>
-    </html>
-  `;
+const app = new Hono();
+
+// Helper functions for rendering templates ===================================
+const hbsCache = new Map<string, HandlebarsTemplateDelegate>();
+async function hbs(path: string, context: object): Promise<string> {
+  path = await Deno.realPath(path);
+  if (hbsCache.has(path)) {
+    return (hbsCache.get(path)!)(context);
+  } else {
+    const compiled = Handlebars.compile(await Deno.readTextFile(path));
+    hbsCache.set(path, compiled);
+    return compiled(context);
+  }
+}
+
+const styleTag = `<style>${Deno.readTextFileSync("static/styles.css")}</style>`;
+async function renderTemplate(template: string, context: object) {
+  return await hbs("views/base.hbs", {
+    content: new Handlebars.SafeString(await hbs(`views/${template}`, context)),
+    styles: new Handlebars.SafeString(styleTag),
+  });
+}
+
+// Routes =====================================================================
+
+app.get("/", async (c) => {
+  const html = await renderTemplate("index.hbs", {
+    percentUpNow: 100,
+  });
+  return c.html(html);
 });
 
-const app = new Application();
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.get("*", serveStatic({ root: "./static/" }));
 
-app.listen({ port: 8080 });
+Deno.serve(app.fetch);
